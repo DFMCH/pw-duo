279a47da623c6fb7b53cd3b29a1eed10542d8bae
tmb384 <tmb384@tesla.fammed.wisc.edu>
2019-04-12 10:04:14 -0500

    removed data structures and macros and added pw-duo.h

--- pw-duo.c
+++ pw-duo.c
@@ -12,6 +12,7 @@
 #include "slap.h"      /* for struct SlapReply */
 #include "lutil_sha1.h"
 #include "duo.h"
+#include "pw-duo/pw-duo.h"
 
 #include <sasl/sasl.h>
 #include <sasl/saslplug.h>
@@ -28,43 +29,6 @@ static const struct berval scheme_duo_ssha1 = BER_BVC("{DUO+SSHA}");
 
 /* not sure this is needed. Some password modules use it, others don't */
 static ldap_pvt_thread_mutex_t pw_duo_mutex;
-
-/* holds duo config/keys */
-struct DUO_CONF {
-   char *ikey;
-   char *skey;
-   char *host;
-};
-
-/*
- * macros for use with duo_auth
- */
-/* available authentication options, returned from duo, start with this text and end with a 1 (for now I guess) */
-#define LABEL_PREFIX_PUSH     "push"
-#define LABEL_PREFIX_SMS      "sms"
-#define LABEL_PREFIX_PHONE    "phone"
-#define DUO_AUTH_FACTOR       "prompt"
-#define DUO_AUTH_IPADDR       "1.2.3.4"   /* if we can get the IP of the host requesting auth it would go here */
-
-#define MODULE_NAME           "pw-duo"
-#define TAG                   "chk_duo()"
-#define DUO_LOGIN_CFG         "/etc/duo/login_duo.conf"  /* use login_duo for DUO keys */
-#define DUO_TIMEOUT           20
-
-/* just use AUTH_MODE_PUSH for now. maybe later this could be specified in the username/password input
- * TODO: could parse the option from username or password.
- * it's a non-interactive auth, so which mode to use?
- * eg: 302233,username or 302303,Pa55w0rd
- * eg: sms,username or sms,Pa55w0rd
- * eg: phone,username or phone,Pa55w0rd
- */
-#define AUTH_MODE_PUSH     100
-#define AUTH_MODE_SMS      101
-#define AUTH_MODE_PHONE    102
-#define AUTH_MODE_PCODE    103
-
-#define PASS_HASH_SEPARATOR '@'  /* used in LDAP userPassword attribute for SSHA auth */
-
 
 /* from openldap-2.4.42+dfsg/servers/slapd/sasl.c  below vvvv */
 #ifdef HAVE_CYRUS_SASL
